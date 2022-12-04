ce71bd0070c20527f945efd6cf56adcff87f536f
seonjuhwang <119748547+seonjuhwang@users.noreply.github.com>
2022-12-04 20:47:26 +0900

    다시

--- api.js
+++ api.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 var cors = require('cors');
+const port = 3000;
 
 app.use(cors());
 const server = app. listen(3000,()=> {
-    console.log('Start Server : localhost:3001');
+    console.log('Start Server : localhost:3000');
 });
 
-app.get('/api/carnumber/:type',async (req,res)=>{
+app.get('/api/carnumber/:type', async(req,res) => {
     let{
         type
     } = req.params;
